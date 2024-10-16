4e8e47d285aeeef6995687b5b1ab267a23748538
Bono Fox <microfuchs@outlook.com>
2024-10-16 15:01:30 +0200

    🎉 Neue Komponente für Lebenslauf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LebenslaufComponent } from './lebenslauf/lebenslauf.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cv',
+    component: LebenslaufComponent,
+  },
+];
---
+++ src/app/lebenslauf/lebenslauf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lebenslauf',
+  standalone: true,
+  imports: [],
+  templateUrl: './lebenslauf.component.html',
+  styleUrl: './lebenslauf.component.scss',
+})
+export class LebenslaufComponent {}
