39c5b8540e4e3dc857e1dd10ffec1072a629d487
Bono Fox <microfuchs@outlook.com>
2024-10-27 13:15:16 +0100

    ♻️ Schrägen CI-Rahmen als separate Komponente extrahiert
    - soll im Portfolio nicht dargestellt werden
    - Ziel für den Rahmen sind folgende Komponenten:
      - Lebenslauf
      - Anschreiben
      - Seitennavigation/"Linktree"

---
+++ src/app/frame/frame.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-frame',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './frame.component.html',
+  styleUrl: './frame.component.scss',
+})
+export class FrameComponent {}
--- src/app/lebenslauf/lebenslauf.component.ts
+++ src/app/lebenslauf/lebenslauf.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FrameComponent } from '../frame/frame.component';
 
 @Component({
   selector: 'app-lebenslauf',
   standalone: true,
-  imports: [],
+  imports: [FrameComponent],
   templateUrl: './lebenslauf.component.html',
   styleUrl: './lebenslauf.component.scss',
 })
