ca7e5b8b2e5939736f79660dc4c831646420efd1
Bono Fox <microfuchs@outlook.com>
2024-10-27 12:24:31 +0100

    🎉 Komponente für Portfolio hinzugefügt

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * Die Portfoliokomponente hat die Aufgabe, eine schicke, moderne und grafische
+ * Darstellung meiner Fähigkeiten und Qualitäten bereitzustellen.
+ */
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.scss',
+})
+export class PortfolioComponent {}
