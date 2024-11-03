e8f147c926012b1c2a567b112cb5a02c48e90c63
Bono Fox <microfuchs@outlook.com>
2024-11-03 21:43:54 +0100

    ✨ Portfoliosektion für Startseite hinzugefügt

---
+++ src/app/portfolio/data/sektionen/sektion-intro.ts
@@ -0,0 +1,9 @@
+import { PortfolioSektion } from '../../model/portfolio-sektion';
+
+export const sektionIntro: PortfolioSektion = {
+  sektionsart: 'titel',
+  domId: 'sektion-intro',
+  background: { type: 'image', imageUrl: 'intellij-background.png' },
+  titel: 'Bono Fox',
+  subtitle: 'Softwareentwicklung ist mein Handwerk.',
+};
