803cda9bd41ba3626aceb95a0d87ffdfe77b2d31
Bono Fox <microfuchs@outlook.com>
2024-11-03 21:38:49 +0100

    🔧 Startseite ist die Portfolio-Komponente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LebenslaufComponent } from './lebenslauf/lebenslauf.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 export const routes: Routes = [
+  { path: '', component: PortfolioComponent },
   {
     path: 'cv',
     component: LebenslaufComponent,
