988933b286fb547863d62f55231f059d51a28d0d
Bono Fox <microfuchs@outlook.com>
2024-11-03 21:53:50 +0100

    🍱 IDE-Hintergrundbild hinzugefügt

---
+++ src/app/portfolio/data/images/ide-background.ts
@@ -0,0 +1,17 @@
+import { PortfolioImage } from '../../model/portfolio-image';
+
+export const ideBackground: PortfolioImage = {
+  src: 'intellij-background.png',
+  alt: 'Ein verschwommenes Bild einer IDE, in der die hiesige Website entwickelt wird',
+
+  width: 1917,
+  height: 1156,
+  quelle: {
+    quelleName: 'Eigene Aufnahme',
+    url: 'https://www.jetbrains.com/de-de/webstorm/',
+    autor: 'Bono Fox',
+  },
+
+  beschreibungstext:
+    'Ich mag die Arbeit in den Entwicklungsumgebungen von Jetbrains',
+};
