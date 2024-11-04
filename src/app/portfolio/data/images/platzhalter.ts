19cc38be93655ef2709cf5352eb7323e938efe77
Bono Fox <microfuchs@outlook.com>
2024-11-04 22:53:19 +0100

    🍱 Allgemeines Platzhalter-Asset hinzugefügt

---
+++ src/app/portfolio/data/images/platzhalter.ts
@@ -0,0 +1,16 @@
+import { PortfolioImage } from '../../model/portfolio-image';
+
+export const platzhalterBild: PortfolioImage = {
+  src: 'platzhalter.png',
+  alt: '',
+
+  width: 128,
+  height: 128,
+  quelle: {
+    quelleName: 'Wikimedia Commons',
+    url: 'https://commons.wikimedia.org/wiki/File:Missing_Texture.png',
+    autor: 'Valve Software',
+  },
+
+  beschreibungstext: 'Diese Grafik fehlt noch!',
+};
