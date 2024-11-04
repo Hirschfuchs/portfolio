15e7a32c620dfd3ed3345b3e63447f0fb82c2ae5
Bono Fox <microfuchs@outlook.com>
2024-11-04 22:52:41 +0100

    ✨ Sektion Fancy hinzugefügt

--- src/app/portfolio/data/sektionen/portfolio-sektionen.ts
+++ src/app/portfolio/data/sektionen/portfolio-sektionen.ts
@@ -1,8 +1,10 @@
 import { PortfolioSektion } from '../../model/portfolio-sektion';
 import { sektionCraftsmanship } from './sektion-craftsmanship';
+import { sektionFancy } from './sektion-fancy';
 import { sektionIntro } from './sektion-intro';
 
 export const portfolioSektionen: ReadonlyArray<PortfolioSektion> = [
   sektionIntro,
   sektionCraftsmanship,
+  sektionFancy,
 ];
