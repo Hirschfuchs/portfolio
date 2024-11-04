fef8bc26240d4b025ae155f54759cf29094cec33
Bono Fox <microfuchs@outlook.com>
2024-11-04 22:53:35 +0100

    🍱 Grafik für Sektion "Extra Meile"

---
+++ src/app/portfolio/data/images/aufraumaktion.ts
@@ -0,0 +1,16 @@
+import { PortfolioImage } from '../../model/portfolio-image';
+
+export const aufraumaktion: PortfolioImage = {
+  src: 'aufraeumaktion-blurry.jpg',
+  alt: 'Die Umrisse eines sortierten Zeitungsständers sind erkennbar',
+
+  width: 4032,
+  height: 3024,
+  quelle: {
+    quelleName: 'Eigene Aufnahme',
+    autor: 'Bono Fox',
+  },
+
+  beschreibungstext:
+    'Wir haben unter anderem die auf den Esstischen verstreuten Zeitungen auf dem Zeitungsständer gruppiert und nach Datum sortiert',
+};
