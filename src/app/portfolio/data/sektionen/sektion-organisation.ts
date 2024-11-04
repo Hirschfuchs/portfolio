f93ab0a919e7314d9c67507cde69043e72bdae2a
Bono Fox <microfuchs@outlook.com>
2024-11-04 22:55:48 +0100

    ✨ Sektion Projektmanagement hinzugefügt

---
+++ src/app/portfolio/data/sektionen/sektion-organisation.ts
@@ -0,0 +1,14 @@
+import { PortfolioTexteMitBildernSektion } from '../../model/portfolio-sektion';
+
+export const sektionOrganisation: PortfolioTexteMitBildernSektion = {
+  sektionsart: 'texte-mit-bildern',
+  domId: 'sektion-projektmanagement-team-organisation',
+  background: { type: 'class', class: 'bg-grau' },
+  head: {
+    titel: 'Projektmanagement & Team-Organisation',
+    text: '', // TODO
+  },
+  textBildKombinationen: [
+    // TODO
+  ],
+};
