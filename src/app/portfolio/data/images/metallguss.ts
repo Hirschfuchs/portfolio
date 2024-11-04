804201019749555856db24861c8acbd563e98e8d
Bono Fox <microfuchs@outlook.com>
2024-11-04 22:50:28 +0100

    🍱 Grafiken für Sektion "Craftsmanship"

---
+++ src/app/portfolio/data/images/metallguss.ts
@@ -0,0 +1,17 @@
+import { PortfolioImage } from '../../model/portfolio-image';
+
+export const metallguss: PortfolioImage = {
+  src: 'acetylene-1239328_1920.jpg',
+  alt: 'Metall wird eingeschmolzen',
+
+  width: 1920,
+  height: 1330,
+  quelle: {
+    quelleName: 'Pixabay',
+    url: 'https://pixabay.com/de/photos/acetylen-fackel-schwei%C3%9Fen-schwei%C3%9Fer-1239328/',
+    autor: 'Robert Owen-Wahl',
+  },
+
+  beschreibungstext:
+    'Die Entwicklung guten Codes ist genauso ein Handwerk, wie das Backen eines guten Brots oder die Verarbeitung von Metall',
+};
---
+++ src/app/portfolio/data/images/schmied.ts
@@ -0,0 +1,14 @@
+import { PortfolioImage } from '../../model/portfolio-image';
+
+export const schmied: PortfolioImage = {
+  src: 'craft-1423803_1920.jpg',
+  alt: 'Schmied bearbeitet ein Schwert im Feuer',
+
+  width: 1920,
+  height: 1535,
+  quelle: {
+    quelleName: 'Pixabay',
+    url: 'https://pixabay.com/de/photos/handwerk-schmiedefeuer-arbeiten-1423803/',
+    autor: 'Peter Freitag',
+  },
+};
