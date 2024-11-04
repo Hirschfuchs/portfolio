4df04675ff6f31597c673b336069143a7301984f
Bono Fox <microfuchs@outlook.com>
2024-11-04 22:54:07 +0100

    ✨ Sektion Extra Meile hinzugefügt

--- src/app/portfolio/data/sektionen/portfolio-sektionen.ts
+++ src/app/portfolio/data/sektionen/portfolio-sektionen.ts
@@ -1,5 +1,6 @@
 import { PortfolioSektion } from '../../model/portfolio-sektion';
 import { sektionCraftsmanship } from './sektion-craftsmanship';
+import { sektionExtraMeile } from './sektion-extra-meile';
 import { sektionFancy } from './sektion-fancy';
 import { sektionIntro } from './sektion-intro';
 
@@ -7,4 +8,5 @@ export const portfolioSektionen: ReadonlyArray<PortfolioSektion> = [
   sektionIntro,
   sektionCraftsmanship,
   sektionFancy,
+  sektionExtraMeile,
 ];
