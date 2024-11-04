c6320345b29e83e9fb97fb3b1636269c88b2f3e4
Bono Fox <microfuchs@outlook.com>
2024-11-04 22:50:28 +0100

    🍱 Grafiken für Sektion "Craftsmanship"

---
+++ src/app/portfolio/data/images/tastatur.ts
@@ -0,0 +1,14 @@
+import { PortfolioImage } from '../../model/portfolio-image';
+
+export const tastatur: PortfolioImage = {
+  src: 'keyboard-915520_1920.jpg',
+  alt: 'Nahaufnahme einer beleuchteten Tastatur',
+
+  width: 1920,
+  height: 1272,
+  quelle: {
+    quelleName: 'Pixabay',
+    url: 'https://pixabay.com/de/photos/klaviatur-pc-rechner-915520/',
+    autor: 'Chris Ciapala',
+  },
+};
