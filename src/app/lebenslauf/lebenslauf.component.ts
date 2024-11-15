3b3a339d90f04622472fcc4b154403592ab6daba
Bono Fox <microfuchs@outlook.com>
2024-11-15 21:58:12 +0100

    ✨ Lebenslauf zeigt zweigeteilte Ansicht an

--- src/app/lebenslauf/lebenslauf.component.ts
+++ src/app/lebenslauf/lebenslauf.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FrameComponent } from '../frame/frame.component';
+import { Split2Component } from '../split-2/split-2.component';
 
 @Component({
   selector: 'app-lebenslauf',
   standalone: true,
-  imports: [FrameComponent],
+  imports: [FrameComponent, Split2Component],
   templateUrl: './lebenslauf.component.html',
   styleUrl: './lebenslauf.component.scss',
 })
