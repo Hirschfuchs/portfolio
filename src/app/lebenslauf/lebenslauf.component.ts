e5987af2769c25a78de637f3e73f1ff1e6cd3ae2
Bono Fox <microfuchs@outlook.com>
2024-11-15 22:09:55 +0100

    ✨ Lebenslaufbild hinzugefügt

--- src/app/lebenslauf/lebenslauf.component.ts
+++ src/app/lebenslauf/lebenslauf.component.ts
@@ -1,12 +1,18 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { FrameComponent } from '../frame/frame.component';
 import { Split2Component } from '../split-2/split-2.component';
+import { ConfigurationService } from './configuration.service';
 
 @Component({
   selector: 'app-lebenslauf',
   standalone: true,
-  imports: [FrameComponent, Split2Component],
+  imports: [FrameComponent, Split2Component, NgOptimizedImage],
   templateUrl: './lebenslauf.component.html',
   styleUrl: './lebenslauf.component.scss',
 })
-export class LebenslaufComponent {}
+export class LebenslaufComponent {
+  constructor(private readonly configurationService: ConfigurationService) {}
+
+  protected primaryColor = this.configurationService.getPrimaryColor();
+}
