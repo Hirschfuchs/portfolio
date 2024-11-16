b8774390888f10d79bcafdd5e95d5e86aff229a1
Bono Fox <microfuchs@outlook.com>
2024-11-16 01:03:05 +0100

    ✨ Skilllevel können dargestellt werden

--- src/app/lebenslauf/key-value-pair/key-value-pair.component.ts
+++ src/app/lebenslauf/key-value-pair/key-value-pair.component.ts
@@ -1,9 +1,16 @@
 import { Component, input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+enum IconState {
+  EMPTY,
+  HALF,
+  FULL,
+}
 
 @Component({
   selector: 'app-key-value-pair',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './key-value-pair.component.html',
   styleUrl: './key-value-pair.component.scss',
 })
@@ -11,4 +18,46 @@ export class KeyValuePairComponent {
   public readonly key = input.required<string>();
   public readonly textValue = input<string | undefined>();
   public readonly levelValue = input<string | undefined>();
+
+  protected getLevelStars() {
+    let stars: IconState[] = [];
+    if (this.levelValue()?.startsWith('1')) {
+      stars.push(IconState.FULL);
+    }
+    if (this.levelValue()?.startsWith('2')) {
+      stars.push(IconState.FULL, IconState.FULL);
+    }
+    if (this.levelValue()?.startsWith('3')) {
+      stars.push(IconState.FULL, IconState.FULL, IconState.FULL);
+    }
+    if (this.levelValue()?.startsWith('4')) {
+      stars.push(
+        IconState.FULL,
+        IconState.FULL,
+        IconState.FULL,
+        IconState.FULL,
+      );
+    }
+    if (this.levelValue()?.startsWith('5')) {
+      return [
+        IconState.FULL,
+        IconState.FULL,
+        IconState.FULL,
+        IconState.FULL,
+        IconState.FULL,
+      ];
+    }
+
+    if (this.levelValue()?.endsWith('5')) {
+      stars.push(IconState.HALF);
+    }
+
+    while (stars.length < 5) {
+      stars.push(IconState.EMPTY);
+    }
+
+    return stars;
+  }
+
+  protected IconState = IconState;
 }
